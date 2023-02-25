9e54ef948bebe5c34a8883b4b3568188f0132645
Kcepriu <kcepriu@gmail.com>
2023-02-25 19:42:12 +0200

    start less

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,16 +1,30 @@
-import { CocktailsList } from '../components/CocktailsList';
-import { Section } from '../components/Section';
-import { Loader } from '../components/Loader';
+import { CocktailsList } from "../components/CocktailsList";
+import { Section } from "../components/Section";
+import { Loader } from "../components/Loader";
+import { useEffect, useState } from "react";
+import { getTrendingCocktails } from "../api/cocktail-service";
 
 export const Home = () => {
+  const [cocktails, setCocktails] = useState([]);
+
+  useEffect(() => {
+    async function fetchTrendingCocktails() {
+      const cocktail = await getTrendingCocktails();
+      const drinks = cocktail.map((element) => element.drinks[0]);
+      setCocktails(drinks);
+    }
+
+    fetchTrendingCocktails();
+  }, []);
+
   return (
     <>
       <Section>
-        <h1 className='text-center font-black text-gray-700 text-4xl mb-10'>
+        <h1 className="text-center font-black text-gray-700 text-4xl mb-10">
           Trending cocktails
         </h1>
 
-        {/* <CocktailsList cocktails={cocktails} /> */}
+        <CocktailsList cocktails={cocktails} />
       </Section>
     </>
   );
